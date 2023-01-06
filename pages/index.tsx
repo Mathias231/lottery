0d909859966b202950b50bd5216c901ed7267365
Mathias231 <mathias.krogh@outlook.com>
2023-01-06 14:59:41 +0100

    Importing Components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,39 @@
+// General Imports
+import { useState, useRef } from 'react';
 import Head from 'next/head';
+import Players from '../components/players';
+import Wheel from '../components/wheel/wheelData';
+import Winner from '../components/winner';
+
+// Importing Interfaces
+import { IPlayer, IPlayerHandle } from '../interface/playerGlobal.interface';
+import { IWheelHandle } from '../interface/wheelGlobal.interface';
 
 export default function Home() {
+  // Declaring states & refs
+  const [spinning, setSpinning] = useState(false);
+  const [players, setPlayers] = useState<IPlayer[]>([]);
+  const [winnerName, setWinnerName] = useState<null | string>(null);
+  const playersRef = useRef<IPlayerHandle>(null);
+  const wheelRef = useRef<IWheelHandle>(null);
+
+  // Functions
+  const changeSpinning = (spinning: boolean) => {
+    setSpinning(spinning);
+  };
+
+  const changePlayers = (player: IPlayer[]) => {
+    setPlayers(player);
+  };
+
+  const removeWinner = (playerName: string) => {
+    playersRef.current?.removePlayer(playerName);
+  };
+
+  const displayWinner = (winnerName: string | null) => {
+    setWinnerName(winnerName);
+  };
+
   return (
     <>
       <Head>
@@ -9,7 +42,41 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/lottery.ico" />
       </Head>
-      <main></main>
+      <main>
+        <div className="container">
+          <div className="leftContainer">
+            {/* <h1>Velg antall deltagere</h1>  Unhide this to push down all button to center and remove <h1> in players with same text as this <h1>*/}
+            <Players
+              disabled={spinning}
+              changePlayers={changePlayers}
+              ref={playersRef}
+            />
+          </div>
+          <div className="rightContainer">
+            {/* <h1>Who is the winner today?</h1> */}
+            <div className="wheel-div">
+              <Wheel
+                changeSpinning={changeSpinning}
+                items={players.map((item) => item.NAME)}
+                disabled={players.length === 0}
+                removeWinner={removeWinner}
+                winnerDisplay={displayWinner}
+                ref={wheelRef}
+              />
+            </div>
+          </div>
+          <div>
+            {winnerName && (
+              <Winner
+                winnerName={winnerName}
+                winnerDisplay={displayWinner}
+                removeWinner={removeWinner}
+                resetSelectedItem={wheelRef.current?.resetSelectedItem}
+              />
+            )}
+          </div>
+        </div>
+      </main>
     </>
   );
 }
