b4ba3a3978bd7592f9bb551049d1f001efef7a34
Mathias231 <mathias.krogh@outlook.com>
2023-01-26 15:12:15 +0100

    fix

--- components/players.tsx
+++ components/players.tsx
@@ -15,6 +15,58 @@ import {
   IPlayersProps,
 } from '../interface/playerGlobal.interface';
 
+interface CheckBoxProps {
+  item: IPlayer;
+  i: number;
+  disabled: boolean;
+  players: IPlayer[];
+  addToPlayers: (player: IPlayer) => void;
+  removePlayer: (playerName: string) => void;
+}
+
+function CheckBox({
+  item,
+  i,
+  disabled,
+  players,
+  addToPlayers,
+  removePlayer,
+}: CheckBoxProps) {
+  const checkboxRef = useRef<HTMLInputElement | null>(null);
+  const checkCheckbox = () => {
+    if (checkboxRef.current?.checked) {
+      addToPlayers(item);
+    }
+
+    if (!checkboxRef.current?.checked) {
+      removePlayer(item.NAME);
+    }
+  };
+  return (
+    <div className="checkboxContainer" key={i}>
+      <input
+        type={'checkbox'}
+        className={'checkbox'}
+        id={i.toString()}
+        ref={checkboxRef}
+        disabled={disabled}
+        onClick={() => {
+          checkCheckbox();
+        }}
+        checked={
+          players.find((player) => {
+            return player.NAME === item.NAME;
+          }) !== undefined
+        }
+        readOnly
+      />
+      <label className={'checkmark'} htmlFor={i.toString()}>
+        {item.NAME}
+      </label>
+    </div>
+  );
+}
+
 const Players: ForwardRefRenderFunction<IPlayerHandle, IPlayersProps> = (
   { changePlayers, disabled },
   ref,
@@ -43,46 +95,14 @@ const Players: ForwardRefRenderFunction<IPlayerHandle, IPlayersProps> = (
 
   useEffect(() => {
     changePlayers(players);
-  }, [players]);
+  }, [players, changePlayers]);
 
   return (
     <div>
       <h1>Velg antall deltagere</h1>
-      {participants.map((item, i) => {
-        const checkboxRef = useRef<HTMLInputElement | null>(null);
-        const checkCheckbox = () => {
-          if (checkboxRef.current?.checked) {
-            addToPlayers(item);
-          }
-
-          if (!checkboxRef.current?.checked) {
-            removePlayer(item.NAME);
-          }
-        };
-        return (
-          <div className="checkboxContainer" key={i}>
-            <input
-              type={'checkbox'}
-              className={'checkbox'}
-              id={i.toString()}
-              ref={checkboxRef}
-              disabled={disabled}
-              onClick={() => {
-                checkCheckbox();
-              }}
-              checked={
-                players.find((player) => {
-                  return player.NAME === item.NAME;
-                }) !== undefined
-              }
-              readOnly
-            />
-            <label className={'checkmark'} htmlFor={i.toString()}>
-              {item.NAME}
-            </label>
-          </div>
-        );
-      })}
+      {participants.map((item, i) =>
+        CheckBox({ item, i, disabled, players, addToPlayers, removePlayer }),
+      )}
     </div>
   );
 };
